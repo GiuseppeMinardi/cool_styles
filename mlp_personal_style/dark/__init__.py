4a14e96689ce329e3bed12bf9dfcde02637c925c
GiuseppeMinardi <38011429+GiuseppeMinardi@users.noreply.github.com>
2025-07-28 23:07:21 +0200

    Refactor code structure for improved readability and maintainability

--- mlp_personal_style/dark/__init__.py
+++ mlp_personal_style/dark/__init__.py
@@ -4,5 +4,10 @@ __current_dir = Path(__file__).parent
 
 charcoal = __current_dir / "charcoal.mplstyle"
 forestdark = __current_dir / "forestdark.mplstyle"
+seadark = __current_dir / "seadark.mplstyle"
 
-available_styles: dict[str, Path] = {"charcoal": charcoal, "forestdark": forestdark}
+available_styles: dict[str, Path] = {
+    "charcoal": charcoal,
+    "forestdark": forestdark,
+    "seadark": seadark,
+}
